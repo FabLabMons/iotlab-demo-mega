a943011903d01400469a8fbf11cb07a2db7ee06d
François Roland <fr.roland@gmail.com>
2018-08-28 10:54:20 +0200

    Read temperature and humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #define LED_SALON 2
 #define LED_EATING_ROOM 3
@@ -9,12 +12,17 @@
 #define LED_KITCHEN 6
 
 #define LIQUID_SENSOR A0
+#define TEMP_HUMID_TYPE DHT22
+#define TEMP_HUMID_SENSOR 22
 
 #define RFID_RST_PIN 49
 #define RFID_SS_PIN 53
 
 MFRC522 rfid(RFID_SS_PIN, RFID_RST_PIN);  // Create MFRC522 instance
 byte rfidUid[4];
+
+DHT_Unified dht(TEMP_HUMID_SENSOR, TEMP_HUMID_TYPE);
+uint32_t delayMS;
 
 void setupLeds();
 void setupSensors();
@@ -23,6 +31,8 @@ void setupRfidReader();
 void readSensors();
 void readRfid();
 
+void printHex(byte *buffer, byte bufferSize);
+
 void setup() {
     Serial.begin(9600);
     while (!Serial) {
@@ -30,9 +40,9 @@ void setup() {
     }
     Serial.println(F("Setup START"));
 
-    setupLeds();
+    //setupLeds();
     setupSensors();
-    setupRfidReader();
+    //setupRfidReader();
 
     Serial.println(F("Setup STOP"));
 }
@@ -47,25 +57,8 @@ void setupLeds() {
 
 void setupSensors() {
     pinMode(LIQUID_SENSOR, INPUT);
+    dht.begin();
 }
-
-void printHex(byte *buffer, byte bufferSize) {
-    for (byte i = 0; i < bufferSize; i++) {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], HEX);
-    }
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-    for (byte i = 0; i < bufferSize; i++) {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], DEC);
-    }
-}
-
 
 void setupRfidReader() {
     SPI.begin();
@@ -73,11 +66,11 @@ void setupRfidReader() {
 }
 
 void loop() {
-    //readSensors();
+    readSensors();
 
-    readRfid();
+    //readRfid();
 
-    delay(1000);
+    delay(2000);
 }
 
 void readSensors() {
@@ -85,6 +78,27 @@ void readSensors() {
 
     Serial.print("liquid level=");
     Serial.println(liquidLevel);
+
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+        Serial.println("Error reading temperature!");
+    }
+    else {
+        Serial.print("Temperature: ");
+        Serial.print(event.temperature);
+        Serial.println(" *C");
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+        Serial.println("Error reading humidity!");
+    }
+    else {
+        Serial.print("Humidity: ");
+        Serial.print(event.relative_humidity);
+        Serial.println("%");
+    }
 }
 
 void readRfid() {
@@ -123,3 +137,11 @@ void readRfid() {
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
 }
+
+void printHex(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
+
