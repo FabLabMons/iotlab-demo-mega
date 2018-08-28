e25e565da1b5148c48c2f471dc2b96e121d699aa
François Roland <fr.roland@gmail.com>
2018-08-28 10:01:06 +0200

    Read RFID card

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 static const uint8_t ledLiving1 = 2;
 static const uint8_t ledLiving2 = 3;
@@ -8,9 +10,21 @@ static const uint8_t ledKitchen = 6;
 
 static const int liquidSensor = A0;
 
+static const uint8_t rfidRST = 22;
+static const uint8_t rfidSS = 23;
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 rfid(SS_PIN, RST_PIN);  // Create MFRC522 instance
+byte nuidPICC[4];
+
 void setupLeds();
 void setupSensors();
+void setupRfidReader();
+
 void readSensors();
+void readRfid();
 
 void setup() {
     Serial.begin(9600);
@@ -21,6 +35,7 @@ void setup() {
 
     setupLeds();
     setupSensors();
+    setupRfidReader();
 
     Serial.println(F("Setup STOP"));
 }
@@ -37,8 +52,33 @@ void setupSensors() {
     pinMode(liquidSensor, INPUT);
 }
 
+void printHex(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
+
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], DEC);
+    }
+}
+
+
+void setupRfidReader() {
+    SPI.begin();
+    rfid.PCD_Init();
+}
+
 void loop() {
-    readSensors();
+    //readSensors();
+
+    readRfid();
 
     delay(1000);
 }
@@ -48,4 +88,41 @@ void readSensors() {
 
     Serial.print("liquid level=");
     Serial.println(liquidLevel);
+}
+
+void readRfid() {
+    if ( ! rfid.PICC_IsNewCardPresent())
+        return;
+
+    if ( ! rfid.PICC_ReadCardSerial())
+        return;
+
+    Serial.print(F("PICC type: "));
+    MFRC522::PICC_Type piccType = MFRC522::PICC_GetType(rfid.uid.sak);
+    Serial.println(MFRC522::PICC_GetTypeName(piccType));
+
+    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+        piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+        piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+        return;
+    }
+
+    if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+        rfid.uid.uidByte[1] != nuidPICC[1] ||
+        rfid.uid.uidByte[2] != nuidPICC[2] ||
+        rfid.uid.uidByte[3] != nuidPICC[3] ) {
+        Serial.println(F("A new card has been detected."));
+
+        for (byte i = 0; i < 4; i++) {
+            nuidPICC[i] = rfid.uid.uidByte[i];
+        }
+
+        Serial.println(F("The NUID tag is:"));
+        Serial.print(F("In hex: "));
+        printHex(rfid.uid.uidByte, rfid.uid.size);
+        Serial.println();
+    }
+
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
 }
